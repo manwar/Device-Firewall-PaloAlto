f35dd5602cfe3ba7430f1b853e553d3d308487fd
Greg Foletta <greg@foletta.org>
2019-03-16 13:33:14 +1100

    Added route object

--- lib/Device/Firewall/PaloAlto/Op/VirtualRouter.pm
+++ lib/Device/Firewall/PaloAlto/Op/VirtualRouter.pm
@@ -8,6 +8,8 @@ use 5.010;
 # PODNAME
 # ABSTRACT: new module
 
+use Device::Firewall::PaloAlto::Op::Route;
+
 use parent qw(Device::Firewall::PaloAlto::JSON);
 
 =encoding utf8
@@ -25,9 +27,28 @@ use parent qw(Device::Firewall::PaloAlto::JSON);
 sub _new {
     my $class = shift;
     my ($api_return) = @_;
-    my %tables = %{$api_return};
+    my %virtual_router;
 
-    return bless \%tables, $class;
+    my @routes = @{$api_return->{result}{entry}};
+    %virtual_router = map { $_->{destination} => Device::Firewall::PaloAlto::Op::Route->_new($_) } @routes;
+
+    return bless \%virtual_router, $class;
+}
+
+=head2 route
+
+    my $default_route = $fw->op->virtual_router('default')->route('0.0.0.0/0');
+
+Returns a L<Device::Firewall::PaloAlto::Op::Route> object if the route exists in the routing table.
+
+Returns 'undef' if the route does not exist in the routing table;
+
+=cut
+
+sub route { 
+    my $self = shift;
+    my ($prefix) = @_;
+    return $self->{$prefix};
 }
 
 
